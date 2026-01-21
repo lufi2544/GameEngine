1b32886dcdbf2f4fce6e440dbb56c81f7b9eca05
Juanes Rayo <lufi2544@gmail.com>
2026-01-21 15:50:27 +0100

    Added build scritps

---
+++ build.bat
@@ -0,0 +1,32 @@
+
+@echo off
+
+:: Set variables
+set THIS_DIR=%~dp0
+set SRC_DIR=%THIS_DIR%/source
+set TESTS_DIR=%~dp0/tests
+set BIN_DIR=bin
+set CFLAGS=/std:c17 /Zi
+set MAIN_FILE=%SRC_DIR%/win32_main.cpp
+set MAYORANA_DIR=%SRC_DIR%/MayoranaFramework/source
+set MAYORANA_REF=%SRC_DIR%/mayorana-reflection/source
+set FLAGS=MAYORANA
+set BINARY_NAME=engine
+set BUILD_DIR=%THIS_DIR%/build
+
+if not exist %BIN_DIR% mkdir %BIN_DIR%
+
+cd %BIN_DIR%
+
+:: Create the bin directory if it doesn't exist
+
+:: Set up MSVC environment (ensure it's the correct script)
+call %BUILD_DIR%/setup_cl_x64.bat
+
+:: Compile with MSVC
+:: Using Fe: in stead of OUT, since we are using unity build.
+cl /Fe:%BINARY_NAME% %CFLAGS% -I%SRC_DIR% -I%TESTS_DIR% -I%MAYORANA_DIR% -I%MAYORANA_REF%/reflection_includes.h %MAIN_FILE% -D%FLAGS% /link /SUBSYSTEM:CONSOLE /nologo
+
+:: print the generated code to a file( we can do this more elegantly with the game files, but is fine ).
+::%BINARY_NAME% > game_generated.h
+
---
+++ source/win32_main.cpp
@@ -0,0 +1,11 @@
+
+/** This is the main entry point to the engine on Windows.  */
+
+#include "mayorana.h"
+
+int main(int arg_num, char** args)
+{
+	mayorana_init();
+	
+	MAYORANA_LOG("Hello from the win32_main, this is history in our lives!!!");
+}
